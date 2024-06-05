e8b8398ddd6fa56ac6326ce96bbc52d3bf740451
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-05 18:40:17 -0400

    Create titleize method

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ class Formatter {
     return (string.replace(/[^a-zA-Z0-9\-\'\ ]/g, ""))
 
   }
+
+  static titleize (string ) {
+    const exception = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    // create an array of the sentence
+    const words = string.split(" ")
+    // iterate through array
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !exception.includes(word)) {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+      } else {
+        return word.toLowerCase()
+      }
+    })
+    // check condition if in expection list
+    // or first word
+    // call function to capitalize
+    return titleizedWords.join(" ")
+    // change array back to string.
+  }
 }
 
-Formatter.sanitize( "ca$@#tching cold" )
+Formatter.titleize( "where the wild things are" )
