20a2cf0e28e30b30dfd3decd40b6817b492aadab
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-05 16:29:41 -0400

    Create static sanitize method

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ class Formatter {
   static capitalize(string) {
     return (string.charAt(0).toUpperCase() + string.slice(1))
   }
+
+  static sanitize(string) {
+    return (string.replace(/[^a-zA-Z0-9\-\'\ ]/g, ""))
+
+  }
 }
 
+Formatter.sanitize( "ca$@#tching cold" )
